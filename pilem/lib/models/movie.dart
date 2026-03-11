61fc838f897dd7f180e751e49be38521c24c65a1
riohermanto2428250069 <riohermanto_2428250069@mhs.mdp.ac.id>
2026-03-11 10:50:41 +0700

    pertemuan 6

--- pilem/lib/models/movie.dart
+++ pilem/lib/models/movie.dart
@@ -9,16 +9,27 @@ class Movie {
 
   Movie({required this.id, required this.title, required this.overview, required this.posterPath, required this.backdropPath, required this.releaseDate, required this.voteAverage});
 
-  factory Movie.fromJson(Map<String, dynamic> json){
+  factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      id : json['id'] ?? '',
-      title : json['title'] ?? '',
-      overview : json['overview'] ?? '',
-      posterPath : json['poster_path'] ?? '',
-      backdropPath : json['backdrop_path'] ?? '',
-      releaseDate : json['release_date'] ?? '',
-      voteAverage : json['vote_average'].toDouble() ?? 0.0,
+      id: json['id'] ?? 0, 
+      title: json['title'] ?? '', 
+      overview: json['overview'] ?? '', 
+      posterPath: json['poster_path'] ?? '', 
+      backdropPath: json['backdrop_path'] ?? '', 
+      releaseDate: json['release_date'] ?? '', 
+      voteAverage: json['vote_average'].toDouble() ?? 0.0,
     );
   }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'poster_path': posterPath,
+      'backdrop_path': backdropPath,
+      'release_date': releaseDate,
+      'vote_average': voteAverage,
+    };
+  }
 }
